d934bc15e8de9f9bb48af826009020adebe5b143
40590148 <157928220+40590148@users.noreply.github.com>
2024-04-20 18:36:36 +0100

    Update PopulationReport.java
    added comments

--- src/main/java/com/napier/sem/PopulationReport.java
+++ src/main/java/com/napier/sem/PopulationReport.java
@@ -1,5 +1,10 @@
 package com.napier.sem;
 
+//class to display population reports
+//name -> name of the place the report is about
+//population -> the population of that place
+//populationCity -> the population that live in a city
+//populationNotCity -> the population that does not live in a city
 public class PopulationReport {
     String name;
     int population;
