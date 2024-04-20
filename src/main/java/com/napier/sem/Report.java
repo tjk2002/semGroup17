dc764a80bd421f061384395c4ed4bc4daa64101f
40590148 <157928220+40590148@users.noreply.github.com>
2024-04-20 18:39:24 +0100

    Update Report.java
    added comments

--- src/main/java/com/napier/sem/Report.java
+++ src/main/java/com/napier/sem/Report.java
@@ -1,5 +1,13 @@
 package com.napier.sem;
 
+//class to display regular reports
+//code -> the code for the place the report is about
+//name -> the name of that place
+//continent -> what continent that place is in
+//region -> what region that place is in
+//district -> what district that palce is in
+//population -> the population of that place
+//capital -> the capital of that place
 public class Report {
     int code;
     String name;
